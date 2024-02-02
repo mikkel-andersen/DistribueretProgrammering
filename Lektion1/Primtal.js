69e62100f7a6c8ac39e785d82bd36ed727590b55
mikkel <mikkel.andersen2810@gmail.com>
2024-02-02 10:31:59 +0100

    Opgave 1.5

--- Lektion1/Primtal.js
+++ Lektion1/Primtal.js
@@ -1,24 +1,32 @@
 
-function printPrime(n)  
-{  
-    for (let i = 2; i <= n; i++) {  
-        if (isPrime(i))  
-            console.log(i + " ");  
-    }  
+function isPrime(n) 
+{ 
+  
+    // Corner cases 
+    if (n <= 1) 
+        return false; 
+    if (n <= 3) 
+        return true; 
+  
+    // This is checked so that we can skip 
+    // middle five numbers in below loop 
+    if (n % 2 == 0 || n % 3 == 0) 
+        return false; 
+  
+    for (let i = 5; i * i <= n; i = i + 6) 
+        if (n % i == 0 || n % (i + 2) == 0) 
+            return false; 
+  
+    return true; 
 } 
-
-function isPrime(n)  
-{  
-    // Corner case  
-    if (n <= 1)  
-        return false;  
   
-    // Check from 2 to n-1  
-    for (let i = 2; i < n; i++)  
-        if (n % i == 0)  
-            return false;  
-  
-    return true;  
+// Function to print primes 
+function printPrime(n) 
+{ 
+    for (let i = 2; i <= n; i++) { 
+        if (isPrime(i)) 
+            console.log(i + " ");  
+    } 
 } 
 
 printPrime(10000);
