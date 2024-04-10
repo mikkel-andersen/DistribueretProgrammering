f05ee7e8388a9554aa35a6bf8c8ee40fc0fea338
mikkel <mikkel.andersen2810@gmail.com>
2024-04-10 09:49:09 +0200

    Eksempler & Opgave14.3

---
+++ Lektion14/Opgave14.1.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const fs = require('fs').promises;
+const port = 8080;
+
+app.use(express.static('*/Eksempler/Node & Express/Node.js og Express/filer'));
+
+function genererLinks(filnavne) {
+    let html = '';
+    for (let filnavn of filnavne) {
+        html += '<a href="' + filnavn + '">' + filnavn + '</a><br>\n';
+    }
+    return html;
+}
+
+app.get('/', async (request, response) => {
+    let filnavne = await fs.readdir(__dirname + '/');
+    let html = genererLinks(filnavne);
+    response.send(html);
+});
+
+app.listen(port);
+
+console.log('Lytter på port ' + port + ' ...');
+
